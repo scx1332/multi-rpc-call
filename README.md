a63c374f79d378ae1269f574ee34b90565ba4a4c
scx1332 <sieciech@gmail.com>
2022-08-17 14:14:40 +0200

    Working on balance provider

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # multi-rpc-call
+
+
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_chainId","params":[],"id":1}' -H "Content-Type: application/json" localhost:8545
+
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_chainId","params":[],"id":1}' -H "Content-Type: application/json" http://54.38.192.207:8545
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_chainId","params":[],"id":1}' -H "Content-Type: application/json" http://51.38.53.113:8545/
--- pure_web3_provider.py
+++ pure_web3_provider.py
@@ -1,9 +1,11 @@
 import requests
 import time
 import json
+import logging
 
-
-
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def _erc20_get_balance_call(token_address, wallet, block):
     strip_wallet = wallet.replace("0x", "")
@@ -17,6 +19,7 @@ def _erc20_get_balance_call(token_address, wallet, block):
             },
             block]
     }
+
 
 def _erc1155_get_balance_call(token_address, wallet, id, block):
     strip_wallet = wallet.replace("0x", "")
@@ -32,12 +35,15 @@ def _erc1155_get_balance_call(token_address, wallet, id, block):
     }
 
 
+class RPCException(Exception):
+    pass
+
+
 class BatchGetBalanceProvider:
-    def __init__(self, endpoint, batch_size, verbose=False):
+    def __init__(self, endpoint, batch_size):
         self._endpoint = endpoint
         self._batch_size = batch_size
         self.number_of_batches_sent = 0
-        self._verbose = verbose
 
     def _multi_call(self, endpoint, call_data_params, max_in_req):
         call_data_array = []
@@ -62,8 +68,7 @@ class BatchGetBalanceProvider:
             start_idx = batch_no * max_in_req
             end_idx = min(len(call_data_array), batch_no * max_in_req + max_in_req)
 
-            if self._verbose:
-                print(f"Requesting responses {start_idx} to {end_idx}")
+            logger.info(f"Requesting responses {start_idx} to {end_idx}")
 
             r = requests.post(endpoint, json=call_data_array[start_idx:end_idx])
             if r.status_code == 413:
@@ -84,6 +89,12 @@ class BatchGetBalanceProvider:
                 if not found_response:
                     raise Exception(f"One of calls response not found {call_data['id']}")
 
+                if 'error' in rpc_resp:
+                    logger.error(f"Error during request number {call_data['id']}:")
+                    logger.error(f"\tCall data: {call_data}")
+                    logger.error(f"\tRPC returned error: {rpc_resp['error']}")
+                    raise RPCException("RPC error, check log for details")
+
                 result_array.append(rpc_resp["result"])
         return result_array
 
@@ -96,10 +107,10 @@ class BatchGetBalanceProvider:
         resp = self._multi_call(self._endpoint, call_data_params, self._batch_size)
         return resp
 
-    def get_erc1155_balances(self, holder_id_pairs, token_address):
+    def get_erc1155_balances(self, holder_id_pairs, token_address, block_no='latest'):
         call_data_params = []
         for holder_id_pair in holder_id_pairs:
-            call_params = _erc1155_get_balance_call(token_address, holder_id_pair[0], holder_id_pair[1], 'latest')
+            call_params = _erc1155_get_balance_call(token_address, holder_id_pair[0], holder_id_pair[1], block_no)
             call_data_params.append(call_params)
 
         resp = self._multi_call(self._endpoint, call_data_params, self._batch_size)
@@ -125,7 +136,6 @@ def test_get_balance():
     resp = p.get_erc20_balances(mumbai_holders, token_address)
     end = time.time()
 
-
     sum_of_glms = 0
     for mumbai_holder_wallet, res in zip(mumbai_holders, resp):
         for res in resp:
@@ -138,7 +148,7 @@ def test_get_balance():
 
 
 def test_erc1155_get_balance():
-    p = BatchGetBalanceProvider('http://54.38.192.207:8545', 10000, verbose=True)
+    p = BatchGetBalanceProvider('http://54.38.192.207:8545', 10000)
 
     holder_id_pairs = []
     for i in range(0, 10000):
@@ -146,11 +156,22 @@ def test_erc1155_get_balance():
     for i in range(0, 10000):
         holder_id_pairs.append(("0x2f252433E8eCa94C77F2b8D573A3a32032530F5C", i))
 
-    resp = p.get_erc1155_balances(holder_id_pairs, "0x430FfE792083B45f8cd55857BC7055172a9c8767")
-    for holder_id_pair, res in zip(holder_id_pairs, resp):
-        amount = int(res, 0)
-        if amount != 0:
-            print(f"Holder: {holder_id_pair[0]} Id: {holder_id_pair[1]} Amount: {amount}")
+    try:
+        resp = p.get_erc1155_balances(holder_id_pairs, "0x430FfE792083B45f8cd55857BC7055172a9c8767", block_no='latest')
+        for holder_id_pair, res in zip(holder_id_pairs, resp):
+            amount = int(res, 0)
+            if amount != 0:
+                print(f"Holder: {holder_id_pair[0]} Id: {holder_id_pair[1]} Amount: {amount}")
+    except RPCException as ex:
+        logger.error(f"{ex}")
+        pass
+
+    # test error report
+    try:
+        resp = p.get_erc1155_balances(holder_id_pairs, "0x430FfE792083B45f8cd55857BC7055172a9c8767", block_no='0x5555')
+    except RPCException as ex:
+        logger.info(f"Node should report error that trie node is missing")
+        pass
 
 
 if __name__ == "__main__":
